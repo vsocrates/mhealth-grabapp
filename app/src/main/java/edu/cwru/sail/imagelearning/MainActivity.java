c1292e0335c176f1526e80e3d7a0e7b86b78270f
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2017-09-24 17:12:00 -0400

    committing changes

--- app/src/main/java/edu/cwru/sail/imagelearning/MainActivity.java
+++ app/src/main/java/edu/cwru/sail/imagelearning/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.VelocityTracker;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -24,6 +25,7 @@ public class MainActivity extends Activity {
 	private final String imgDir = Environment.getExternalStorageDirectory().toString() + "/DCIM/";
 	//Make sure that this part is dynamically defined by the Browse Folder and
 	// your CSV file name is "THE_SAME_FOLDER_NAME.csv"
+	private VelocityTracker mVelocityTracker = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -99,7 +101,10 @@ public class MainActivity extends Activity {
 
 				Picasso.with(this).load(data.getData()).resize(1000, 1000)
 						.into(iv);
-			}
+				Bitmap b = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888);
+				Canvas c = new Canvas(b);
+				c.drawBitmap(b, new Matrix(), null);
+		}
 
 		}
 	}
@@ -124,6 +129,47 @@ public class MainActivity extends Activity {
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int index = event.getActionIndex();
+		int action = event.getActionMasked();
+		int pointerId = event.getPointerId(index);
+		Log.d("tracking motion", "tracking the motion");
+
+		switch(action) {
+			case MotionEvent.ACTION_DOWN:
+				if(mVelocityTracker == null) {
+					// Retrieve a new VelocityTracker object to watch the
+					// velocity of a motion.
+					mVelocityTracker = VelocityTracker.obtain();
+				}
+				else {
+					// Reset the velocity tracker back to its initial state.
+					mVelocityTracker.clear();
+				}
+				// Add a user's movement to the tracker.
+				mVelocityTracker.addMovement(event);
+				break;
+			case MotionEvent.ACTION_MOVE:
+				mVelocityTracker.addMovement(event);
+				// When you want to determine the velocity, call
+				// computeCurrentVelocity(). Then call getXVelocity()
+				// and getYVelocity() to retrieve the velocity for each pointer ID.
+				mVelocityTracker.computeCurrentVelocity(1000);
+				// Log velocity of pixels per second
+				// Best practice to use VelocityTrackerCompat where possible.
+				Log.d("", "X velocity: " + mVelocityTracker.getXVelocity());
+				Log.d("", "Y velocity: " + mVelocityTracker.getYVelocity());
+				break;
+			case MotionEvent.ACTION_UP:
+			case MotionEvent.ACTION_CANCEL:
+				// Return a VelocityTracker object back to be re-used by others.
+				mVelocityTracker.recycle();
+				break;
+		}
+		return true;
 	}
 
 }
