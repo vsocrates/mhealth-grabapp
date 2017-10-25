eb88631ad2a58462645721308ad84debe10b3c99
Vimig Socrates <vimig.socrates@gmail.com>
2017-10-25 06:48:06 -0400

    added lab 3

---
+++ Lab3/read_dataset.lua
@@ -0,0 +1,59 @@
+
+require "torch"
+require "image"
+require "math"
+
+classes = {1,2,3,4,5,6,7,8,9,10,11,12}
+classes_names = {'0','1','2','3','4','5','6','7','8','9','10','11'}
+
+
+function loadtxt(datafile, labelfile)
+	local datafile,dataerr=io.open(datafile,"r")
+	local labelfile,labelerr=io.open(labelfile,"r")
+	if dataerr then print("Open data file Error")	end
+	if labelerr then print("Open label file Error")	end
+	local index = 0
+        local dataset={}
+	while true do
+		local dataline=datafile:read('*l')
+		local labelline=labelfile:read('*l')
+		if dataline == nil then break end
+		index=index+1
+		--print (index)
+		data1=dataline:split(' ')
+		data=torch.Tensor(561,1)
+		--data={}
+		for i=1,561 do
+			data[i]=tonumber(data1[i])
+		end
+		label1=labelline:split(' ')
+		label=torch.Tensor(1)
+		--label={}
+		for i=1,table.getn(label1) do
+			label[i]=tonumber(label1[i])
+		end     
+		dataset[index]={data,label}
+	end
+	function dataset:size() return index end
+	return dataset
+end
+
+local training_dataset = loadtxt('X_train.txt','y_train.txt')
+local testing_dataset   = loadtxt('X_test.txt','y_test.txt')
+
+return training_dataset, testing_dataset, classes, classes_names
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lab3/train.lua
@@ -0,0 +1,241 @@
+require "torch"
+require "nn"
+require "math"
+
+
+-- global variables
+learningRate = 0.01
+innerIteration = 1
+outerIteration = 1
+-- fraction = 0.01
+batchsize = 500
+
+function subset(dataset, head, tail)
+  local sub = {}
+  local index = 0
+  for i = head, tail do
+    index = index + 1
+    sub[index] = dataset[i]
+  end
+  function sub:size() return index end
+
+  return sub
+end
+
+-- here we set up the architecture of the neural network
+-- input raw data 561 * 1
+--ANN with two hidden layer
+function create_network()
+
+  local ann = nn.Sequential(); 
+
+  ann:add(nn.View(561 * 1))
+  ann:add(nn.Linear(561, 300))
+  ann:add(nn.ReLU())
+
+  ann:add(nn.Linear(300, 100))
+  ann:add(nn.ReLU())
+
+  --output layer
+  ann:add(nn.Linear(100, 12))
+  ann:add(nn.ReLU())
+  ann:add(nn.LogSoftMax())
+
+  return ann
+end
+
+--[[
+--ANN with no hidden layer
+-- input raw data 561 * 1
+function create_network()
+
+  local ann = nn.Sequential(); 
+
+  --output layer
+  ann:add(nn.View(561 * 1))
+  ann:add(nn.Linear(561, 12))
+  ann:add(nn.ReLU())
+  ann:add(nn.LogSoftMax())
+
+  return ann
+end
+--]]
+
+--[[
+--ANN with one hidden layer
+-- input raw data 561 * 1
+function create_network()
+
+  local ann = nn.Sequential(); 
+
+  --output layer
+  ann:add(nn.View(561 * 1))
+  ann:add(nn.Linear(561, 100))
+  ann:add(nn.ReLU())
+
+  ann:add(nn.Linear(100, 12))
+  ann:add(nn.ReLU())
+  ann:add(nn.LogSoftMax())
+
+  return ann
+end
+--]]
+ 
+--In this function, we update the parameters of network with current_batch.
+function update(network, dataset)
+  local criterion = nn.ClassNLLCriterion()
+  for iteration = 1, innerIteration do
+    print("quux this is inner iteration number " .. iteration)
+    for t = 1, table.getn(dataset) do
+      local example = dataset[t]
+      local input = example[1]
+      local target = example[2]
+      
+      --zero the buffer of gradients.
+      network:zeroGradParameters();
+      --feedforward and backpropagation.
+      network:backward(input, criterion:backward(network:forward(input), target));
+      --update the weights using trained gradients
+      network:updateParameters(learningRate);
+    end
+    --test current model.
+    test_predictor(network, testing_dataset, classes, classes_names)
+  end
+end
+
+--You should modify test_predictor() to get multiple confusion matrices. 
+--print() what you want to observe.
+function test_predictor(predictor, test_dataset, classes, classes_names)
+
+  local mistakes = 0
+  local tested_samples = 0
+
+  for i = 1, table.getn(test_dataset) do
+    local input = test_dataset[i][1]
+    local class_id = test_dataset[i][2]
+    local responses_per_class = predictor:forward(input)
+    local probabilites_per_class = torch.exp(responses_per_class)
+    --Compare the confidence score of all the classes, 
+    --the one with maximum score will be assigned to current test sample.
+    --To draw ROC curve, you can define the misclassification cost vector with entries:
+    --C_{ij}= relative cost of misclassifying a data point in class j to class i.
+    --For instance, classifying a cancerous biopsy as benign is more
+    --critical than classifying a benign biopsy as malignant.
+    --If you are most intersted in class A, you can average confidence score of all the remaining classes and
+    --treat all the remaining classes as one class, namely, not A. 
+    --Then, give misclassification cost to "A" and "not A". For example, if the confidence score of "A" and "not A"
+    --is [5,10] and the misclassification cost of "A" and "not A" is [0.8,0.2], then the true score of "A" is 5*0.8=4 and
+    --the true score of "not A" is 10*0.2=2. Therefore, the current test sample belongs to "A".
+    --By changing the misclassification cost vector, you get multiple confusion matrices, then you get multiple points on ROC space.
+    --In our case, you should fix the weight for "not A", change the weight for "A" from small to large. 
+    local probability, prediction = torch.max(probabilites_per_class, 1)
+
+    if prediction[1] ~= class_id[1] then
+      mistakes = mistakes + 1
+      local label = classes_names[classes[class_id[1]]]
+      local predicted_label = classes_names[classes[prediction[1]]]  
+      --print(i , label , predicted_label )   
+    end
+
+    tested_samples = tested_samples + 1
+  end
+
+  local test_err = mistakes / tested_samples
+  local accuracy = 1 - test_err
+
+  print("accuracy = " .. accuracy)
+end
+
+function train(network)
+  for index=0,batch_number do
+    local dataIndex = index
+    print("batch number:" .. dataIndex)
+    print("---start training---!")
+    local startPoint = 1+dataIndex*batchsize
+    local endPoint = dataIndex*batchsize+batchsize
+    --If endpoint is not exceed the size of the training set,
+    --we update the model using current batch. 
+    if endPoint<train_size then
+      local current_batch = subset(training_dataset, startPoint, endPoint)
+      ---start training---
+      ----------------------------------------------
+      update(network, current_batch)
+      print("---start testing---!")
+      test_predictor(network, testing_dataset, classes, classes_names)
+      print("Finish Training!")
+    --If endpoint is exceed the size of the training set but startPoint is not exceed,
+    --we also update the model using current batch.
+    elseif startPoint<train_size then
+      local current_batch = subset(training_dataset, startPoint, train_size)
+      ---start training---
+      ----------------------------------------------
+      update(network, current_batch)
+      print("---start testing---!")
+      test_predictor(network, testing_dataset, classes, classes_names)
+      print("Finish Training!")
+    --Done.
+    else
+      print('I am done.')
+    end
+  end
+end
+
+-- main routine
+function main()
+  cmd = torch.CmdLine()
+  
+  cmd:text()
+  cmd:text()
+  cmd:text('Training a simple network')
+  cmd:text()
+  cmd:text('Options')
+  cmd:option('-RunFunction',0,'0 is cross validation, 1 is algorithm on the full sample')
+  cmd:option('-LearningRate',0.01,'learning rate')
+  cmd:option('-InnerIteration',5,'set the number of inner iteration')
+  cmd:option('-OuterIteration',2,'set the number of outer iteration')
+  cmd:option('-DescentFunction',1,'0 is stochastic gradient descent, 1 is regular gradient descent')
+  cmd:option('-BatchSize',500,'the number of gradients should be sum')
+  cmd:text()
+
+  params = cmd:parse(arg)
+
+  learningRate = params.LearningRate
+  innerIteration = params.InnerIteration
+  outerIteration = params.OuterIteration
+  batchsize = params.BatchSize
+
+  print("Begin to train!")
+  --load the data
+  training_dataset, testing_dataset, classes, classes_names = dofile('read_dataset.lua')
+  --print(training_dataset)
+  print("Finish Loading!")
+  
+  train_size = training_dataset:size()
+  batch_number = math.modf(train_size/batchsize)+1
+  print("batch_number", batch_number)
+  print("About to shuffle the order !")
+  ----shuffle the order of the whole dataset----
+  local training_data={}
+  local shuffledIndices = {}
+  local f = io.open('order.random')
+  for line in f:lines() do
+    table.insert(shuffledIndices, tonumber(line))
+  end
+  for i=1,training_dataset:size() do
+    local example = training_dataset[shuffledIndices[i]]
+    training_data[i]=example
+  end
+  training_dataset=training_data
+
+  --initalize network
+  network = create_network()
+  --begin to train
+  for maxIterations=1,outerIteration do
+    print("quux this is outer iteration number " .. maxIterations)
+    train(network)
+  end
+  --test the final model
+  test_predictor(network, testing_dataset, classes, classes_names)
+end
+
+main()
