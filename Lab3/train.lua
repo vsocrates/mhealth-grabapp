ab45e4519af9d9752f305a6ad513c98230b464db
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2017-10-29 18:30:11 -0400

    filled in method for test_predictor
    Signed-off-by: Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>

--- Lab3/train.lua
+++ Lab3/train.lua
@@ -108,62 +108,123 @@ end
 function test_predictor(predictor, test_dataset, classes, classes_names)
 
   local mistakes = 0
+  local true_positives = 0
+  local false_positives = 0
+  local true_negatives = 0
+  local false_negatives = 0
+  local actual_positives = 0
+  local actual_negatives = 0
   local tested_samples = 0
-  print("the length of dataset is ", table.getn(test_dataset))
-
-  for i = 1, table.getn(test_dataset) do
-    local input = test_dataset[i][1]
-    local class_id = test_dataset[i][2]
-    local responses_per_class = predictor:forward(input)
-    local probabilites_per_class = torch.exp(responses_per_class)
-    local max_confidence_score = 0.0
-    local max_confidence_score_index = 0
-    for j= 1, 12 do
-      print(probabilites_per_class[j])
-      if tonumber(probabilites_per_class[j]) > max_confidence_score then
-        max_confidence_score = tonumber(probabilites_per_class[j])
-        max_confidence_score_index = j
-      end
-    end
-    local not_max_class_score = 0 
-    for j=1, 12 do
-      if j ~= max_confidence_score_index then
-        not_max_class_score = not_max_class_score + tonumber(probabilites_per_class[j])
-      end
-    end
-    not_max_class_score = not_max_class_score/11
-    print("not max score", not_max_class_score)
-
-
-    --Compare the confidence score of all the classes, 
-    --the one with maximum score will be assigned to current test sample.
-    --To draw ROC curve, you can define the misclassification cost vector with entries:
-    --C_{ij}= relative cost of misclassifying a data point in class j to class i.
-    --For instance, classifying a cancerous biopsy as benign is more
-    --critical than classifying a benign biopsy as malignant.
-    --If you are most intersted in class A, you can average confidence score of all the remaining classes and
-    --treat all the remaining classes as one class, namely, not A. 
-    --Then, give misclassification cost to "A" and "not A". For example, if the confidence score of "A" and "not A"
-    --is [5,10] and the misclassification cost of "A" and "not A" is [0.8,0.2], then the true score of "A" is 5*0.8=4 and
-    --the true score of "not A" is 10*0.2=2. Therefore, the current test sample belongs to "A".
-    --By changing the misclassification cost vector, you get multiple confusion matrices, then you get multiple points on ROC space.
-    --In our case, you should fix the weight for "not A", change the weight for "A" from small to large. 
-    local probability, prediction = torch.max(probabilites_per_class, 1)
-
-    if prediction[1] ~= class_id[1] then
-      mistakes = mistakes + 1
-      local label = classes_names[classes[class_id[1]]]
-      local predicted_label = classes_names[classes[prediction[1]]]  
-      --print(i , label , predicted_label )   
-    end
-
-    tested_samples = tested_samples + 1
+  local thresholds = {}
+  --print("the length of dataset is ", table.getn(test_dataset))
+  for j =1, 10 do
+    thresholds[j] = {j*.1, 1-(j *.1)}
   end
+  thresholds[10] = {0.95, .05}
+  --print(thresholds)
+  for j =1, 10 do
+    for i = 1, table.getn(test_dataset) do
+      local input = test_dataset[i][1]
+      local class_id = test_dataset[i][2]
+      local responses_per_class = predictor:forward(input)
+      local probabilites_per_class = torch.exp(responses_per_class)
+      --print(responses_per_class)
 
-  local test_err = mistakes / tested_samples
-  local accuracy = 1 - test_err
+     local class_A_probability, prediction = torch.max(probabilites_per_class, 1)
+      local not_max_class_score = 0 
+      for j=1, 12 do
+        if j ~= prediction then
+          not_max_class_score = not_max_class_score + tonumber(probabilites_per_class[j])
+        end
+      end
+      not_max_class_score = not_max_class_score/11
 
-  print("accuracy = " .. accuracy)
+      --print("not max score", not_max_class_score)
+
+
+      --Compare the confidence score of all the classes, 
+      --the one with maximum score will be assigned to current test sample.
+      --To draw ROC curve, you can define the misclassification cost vector with entries:
+      --C_{ij}= relative cost of misclassifying a data point in class j to class i.
+      --For instance, classifying a cancerous biopsy as benign is more
+      --critical than classifying a benign biopsy as malignant.
+      --If you are most intersted in class A, you can average confidence score of all the remaining classes and
+      --treat all the remaining classes as one class, namely, not A. 
+      --Then, give misclassification cost to "A" and "not A". For example, if the confidence score of "A" and "not A"
+      --is [5,10] and the misclassification cost of "A" and "not A" is [0.8,0.2], then the true score of "A" is 5*0.8=4 and
+      --the true score of "not A" is 10*0.2=2. Therefore, the current test sample belongs to "A".
+      --By changing the misclassification cost vector, you get multiple confusion matrices, then you get multiple points on ROC space.
+      --In our case, you should fix the weight for "not A", change the weight for "A" from small to large. 
+      local probability, prediction = torch.max(probabilites_per_class, 1)
+
+      local temp_predicted_value = 0
+
+      local temp_predicted_value_1 = thresholds[j][1] * class_A_probability[1]
+      local temp_predicted_value_2 = thresholds[j][2] * not_max_class_score
+      if temp_predicted_value_1 > temp_predicted_value_2 then
+        temp_predicted_value = 1
+      else
+        temp_predicted_value = 0
+      end
+
+      local label = class_id[1]
+      local predicted_label = prediction[1]
+      local temp_label_value = 0
+      --local temp_predicted_value = 0
+      if tonumber(label) == 4 then
+        temp_label_value = 1
+      else
+        temp_label_value = 0
+      end
+      -- if tonumber(predicted_label) == 4 then
+      --   temp_predicted_value = 1
+      -- else
+      --   temp_predicted_value = 0
+      -- end
+      --print("printing temp label ", temp_label_value)
+      --print("printing temp predicted ", temp_predicted_value)
+
+      if temp_label_value ~= temp_predicted_value then
+        --print("they are not equal")
+        mistakes = mistakes + 1
+        if temp_label_value == 1 then
+            false_negatives = false_negatives + 1 
+            actual_positives = actual_positives + 1
+        else 
+          false_positives = false_positives + 1
+          actual_negatives = actual_negatives + 1
+        end
+        --print(i , label , predicted_label )
+      else
+        if temp_label_value == 1 then
+          true_positives = true_positives + 1
+          actual_positives = actual_positives + 1
+        else
+          true_negatives = true_negatives + 1
+          actual_negatives = actual_negatives + 1
+        end
+      end
+      tested_samples = tested_samples + 1
+    end
+
+    local test_err = mistakes / tested_samples
+    local accuracy = 1 - test_err
+    local true_positive_rate = true_positives/actual_positives
+    local true_negative_rate = true_negatives/actual_negatives
+    local false_positive_rate = false_positives/actual_negatives
+
+
+
+    print("accuracy = " .. accuracy)
+    print("true positive rate = ",true_positive_rate)
+    print("false positive rate = ", false_positive_rate )
+    print("specificity = ", true_negative_rate)
+    print("the mistakes are ", mistakes)
+    print("the true negatives are ", true_negatives)
+    print("the false_negatives are ", false_negatives)
+    print("the actual positives are ", actual_positives)
+    print("the actual negatives are ", actual_negatives)
+  end
 end
 
 function train(network)
