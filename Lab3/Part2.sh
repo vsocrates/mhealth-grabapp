a2528441f07ce3b7d06f9ac1dd03e44c085dd4cc
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2017-10-30 05:28:15 -0400

    implemented part 2 and part 3
    Signed-off-by: Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>

---
+++ Lab3/Part2.sh
@@ -0,0 +1,7 @@
+th train_one_hidden.lua -LearningRate 0.01 -InnerIteration 1 -OuterIteration 2 > Exp2_II1_OI2.txt
+th train_one_hidden.lua -LearningRate 0.01 -InnerIteration 1 -OuterIteration 4 > Exp2_II1_OI4.txt
+th train_one_hidden.lua -LearningRate 0.01 -InnerIteration 1 -OuterIteration 6 > Exp2_II1_OI6.txt
+
+th train_one_hidden.lua -LearningRate 0.01 -InnerIteration 2 -OuterIteration 1 > Exp2_II2_OI1.txt
+th train_one_hidden.lua -LearningRate 0.01 -InnerIteration 4 -OuterIteration 1 > Exp2_II4_OI1.txt
+th train_one_hidden.lua -LearningRate 0.01 -InnerIteration 6 -OuterIteration 1 > Exp2_II6_OI1.txt\
